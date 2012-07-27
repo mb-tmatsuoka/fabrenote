36fa945c8702a37a3f3bd11dac25cdd118afba4a
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-07-27 10:37:49 +0900

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabrenote::Application.config.secret_token = '290312d33e96da3c951b28247df176b15843742b524bef331ae013d03e6990ab57ef8ba353adfee4abbf2a7bb6c63b67400c8da6ee131cbae414ce62ef0e4df2'
